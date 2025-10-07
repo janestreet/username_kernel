20671c8d5b83a90bdce30602edc42ccf55ca673d
public-release <as-public-release@janestreet.com>
2025-10-07 02:31:12 +0100

    v0.18~preview.130.55+197

--- src/username.mli
+++ src/username.mli
@@ -1,4 +1,7 @@
+@@ portable
+
 (** A string representation for a user, typically a UNIX username. *)
 
 open! Core
-include String_id.S_with_extras
+
+include%template String_id.S_with_extras [@mode local] [@modality portable]
