a5e171954f3004d76f952125ddd97df6ca2d190c
public-release <as-public-release@janestreet.com>
2026-07-10 17:44:55 +0100

    v0.18~preview.130.106+341

--- src/username.ml
+++ src/username.ml
@@ -3,7 +3,7 @@ open! Core
 let module_name = "Username_kernel.Username"
 
 include
-  (val String_id.make
+  (val (String_id.make [@modality portable])
          ~caller_identity:String_id.legacy_identity
          ~module_name
          ~include_default_validation:true
